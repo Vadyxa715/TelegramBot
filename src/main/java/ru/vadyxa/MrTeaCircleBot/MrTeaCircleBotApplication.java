17d791597f5a3c52cfdf7b7b34ef21a7269bdd67
Vadyxa715 <vadim_kurdin@mail.ru>
2024-08-10 19:19:23 +0300

    init commit

---
+++ src/main/java/ru/vadyxa/MrTeaCircleBot/MrTeaCircleBotApplication.java
@@ -0,0 +1,13 @@
+package ru.vadyxa.MrTeaCircleBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrTeaCircleBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrTeaCircleBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/vadyxa/MrTeaCircleBot/MrTeaCircleBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vadyxa.MrTeaCircleBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrTeaCircleBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
