c458218ac160a4337d0283a169a01e2246b05f51
Vadyxa715 <vadim_kurdin@mail.ru>
2024-08-12 00:38:54 +0300

    VK

--- src/main/java/ru/vadyxa/MrTeaCircleBot/MrTeaCircleBotApplication.java
+++ src/main/java/ru/vadyxa/MrTeaCircleBot/MrTeaCircleBotApplication.java
@@ -1,24 +1,33 @@
 package ru.vadyxa.MrTeaCircleBot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.ApiConstants;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 import ru.vadyxa.MrTeaCircleBot.configuration.ConfigurationBot;
 import ru.vadyxa.MrTeaCircleBot.initbot.Bot;
 
 @SpringBootApplication
+@Slf4j
 public class MrTeaCircleBotApplication {
 
 	public static void main(String[] args) throws TelegramApiException {
 		var ctx =  SpringApplication.run(MrTeaCircleBotApplication.class, args);
+		TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
 		var configurationBot = ctx.getBean(ConfigurationBot.class);
 
 		var bot = new Bot(configurationBot);
 
-		TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-		botsApi.registerBot(bot);
+
+
+		try {
+			botsApi.registerBot(bot);
+		} catch (TelegramApiException e) {
+			log.error(e.getMessage());
+		}
 
 		bot.sendText(configurationBot.getUserId(), "/help");
 
--- src/main/java/ru/vadyxa/MrTeaCircleBot/initbot/Bot.java
+++ src/main/java/ru/vadyxa/MrTeaCircleBot/initbot/Bot.java
@@ -1,22 +1,25 @@
 package ru.vadyxa.MrTeaCircleBot.initbot;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.ComponentScan;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.polls.SendPoll;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.*;
+import org.telegram.telegrambots.meta.api.objects.polls.PollOption;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 import ru.vadyxa.MrTeaCircleBot.configuration.ConfigurationBot;
 
-import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
 @ComponentScan
 @AllArgsConstructor
 public class Bot extends TelegramLongPollingBot {
@@ -24,7 +27,8 @@ public class Bot extends TelegramLongPollingBot {
     public static final String MENU = "/menu";
     public static final String SETTINGS = "/settings";
     public static final String HELP = "/help";
-    public static final String ALL_COMMAND = "\n" + MENU + "\n" + SETTINGS + "\n" + HELP;
+    public static final String CREATE_ANSWER = "/create_answer";
+    public static final String ALL_COMMAND = "\n" + MENU + "\n" + SETTINGS + "\n" + HELP + "\n" + CREATE_ANSWER;
 
 
     private final ConfigurationBot configurationBot;
@@ -34,10 +38,9 @@ public class Bot extends TelegramLongPollingBot {
         var msg = update.getMessage();
         var user = msg.getFrom();
         var userId = user.getId();
+        var chatId = msg.getChatId();
 
-        System.out.println(user.getFirstName() + " with userId=" + userId + " wrote " + msg.getText());
-
-        var txt = msg.getText();
+        log.info("User name: {} with userId: {} wrote message: \"{}\".",user.getFirstName(), userId, msg.getText());
 
         var setButtons = buildButtons();
         var keyBord1 = InlineKeyboardMarkup.builder()
@@ -58,8 +61,11 @@ public class Bot extends TelegramLongPollingBot {
                 case (HELP):
                     sendText(userId, "Список доступных команд: " + ALL_COMMAND);
                     break;
+                case (CREATE_ANSWER):
+                    sendPollInChat(userId, "Тест метода: 'sendPollInChat'", chatId);
+                    break;
                 default:
-                    sendText(userId, "Такой команды не существует");
+                    sendText(userId, "Такой команды не существует. Помощь - " + HELP);
             }
         }
     }
@@ -116,6 +122,60 @@ public class Bot extends TelegramLongPollingBot {
 
     public void checkPidor() {
         sendText(configurationBot.getUserId(), "Пидор дня:  обнаружен!");
+    }
+
+    public void sendPollInChat(Long who, String what, Long chatId) {
+        /**
+         * Type of the entity. Currently, can be:
+         * - “mention” (@username)
+         * - “hashtag” (#hashtag)
+         * - “cashtag” ($USD)
+         * - “bot_command” (/start@jobs_bot)
+         * - “url” (https://telegram.org)
+         * - “email” (do-not-reply@telegram.org)
+         * - “phone_number” (+1-212-555-0123),
+         * - “bold” (bold text)
+         * - “italic” (italic text)
+         * - “underline” (underlined text)
+         * - “strikethrough” (strikethrough text)
+         * - “spoiler” (spoiler message)
+         * - “blockquote” (block quotation)
+         * - “code” (monowidth string)
+         * - “pre” (monowidth block)
+         * - “text_link” (for clickable text URLs)
+         * - “text_mention” (for users without usernames)
+         * - "custom_emoji" (for inline custom emoji stickers)
+         */
+        var messageEntity = MessageEntity.builder()
+                .type("hashtag")
+                .offset(10)
+                .length(10)
+                .text("Over text text text text text text")
+                .build();
+        var entities = new ArrayList<MessageEntity>();
+        entities.add(messageEntity);
+        entities.add(messageEntity);
+        entities.add(messageEntity);
+
+        var message = new Message();
+        message.setEntities(entities);
+
+        SendPoll sendPoll = SendPoll.builder()
+                .chatId(chatId)
+                .question("Стартанет али нет?")
+                .explanationEntity(messageEntity)
+                .option("Ну типо да")
+                .option("Ну типо да")
+                .allowMultipleAnswers(true)
+                .allowSendingWithoutReply(true)
+                .isClosed(false)
+                .build();
+
+        try {
+            execute(sendPoll);                        //Actually sending the message
+        } catch (TelegramApiException e) {
+            log.error(e.getMessage());
+        }
     }
 
 
